0c0025b6a8e86ddca52f354a5750e9cd95d85a63
ooeid <ooeid.omar@gmail.com>
2020-02-16 04:49:57 +0300

    Updates

---
+++ precache-manifest.a87193e07deef735be0c7cc9c8533109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214a43639f21c2be9b55f5235c71aae5",
+    "url": "/speed_typing_game/index.html"
+  },
+  {
+    "revision": "e9e0e693ef57f0aab2f3",
+    "url": "/speed_typing_game/static/css/main.f49f0a0e.chunk.css"
+  },
+  {
+    "revision": "c12df36bb195cfb5563f",
+    "url": "/speed_typing_game/static/js/2.b4288b60.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/speed_typing_game/static/js/2.b4288b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e0e693ef57f0aab2f3",
+    "url": "/speed_typing_game/static/js/main.5bfb6588.chunk.js"
+  },
+  {
+    "revision": "a35b660688851d5ac80f",
+    "url": "/speed_typing_game/static/js/runtime-main.04645725.js"
+  }
+]);
